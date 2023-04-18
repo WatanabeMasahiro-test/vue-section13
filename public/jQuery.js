540b49a793dd90574895df44954f98dd53a4f6f9
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-04-18 15:05:46 +0900

    セクション13終了

---
+++ public/jQuery.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    // f_test();
+
+
+    // function f_test() {
+    //     // alert("I'm testman");
+    //     // return false;
+    // }
+
+});
--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,76 @@
 <template>
-  <div>
-    <router-view></router-view>
-    <p class="bg-primary">{{ test }}</p>
+  <div class="container mt-4">
+    <div class="row">
+      <router-view name="header"></router-view>
+      <!--**
+          * <router-view>は、<transition>または<keep-alive>内での直接使用が非推奨
+          * コンソールで警告が出たため変更
+          **  -->
+      <!-- <Transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </Transition> -->
+      <RouterView v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </RouterView>
+    </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+  import './assets/animate.css/animate.min.css'
+  import './assets/css/style.css'
 
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  },
-  data: () => ({
-      test : 'テスト',
-    }),
-    computed: {
+  export default {
+    name: 'App',
+    components: {
+      // HelloWorld
     },
-    methods: {
-      // testFunc() {
-      //   return false;
-      // },
-      // beforeEnter(el)   {},
-      // enter(el, done)   {},
-      // afterEnter()    {},
-      // enterCancelled(){},
-      // beforeLeave()   {},
-      // leave(el, done)         {},
-      // afterLeave()    {},
-      // leaveCancelled(){},
+    data: () => ({
+        // test : 'テスト',
+      }),
+      computed: {
+      },
+      methods: {
+        // testFunc() {
+        //   return false;
+        // },
+        // beforeEnter()   {},
+        // enter(el, done)   {},
+        // afterEnter()    {},
+        // enterCancelled(){},
+        // beforeLeave()   {},
+        // leave(el, done)         {},
+        // afterLeave()    {},
+        // leaveCancelled(){},
     },
   }
 </script>
 
 <style scoped>
+.link--active {
+  font-size: 20px;
+}
 
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+  /* .fade-enter-to {} */
+  /* .fade-leave-from {} */
+  .fade-leave-active {
 
-  /* 
-  .__-enter-from {}
-  .__-enter-active {}
-  .__-enter-to {}
-  .__-leave-from {}
-  .__-leave-active {}
-  .__-leave-to {}
-  @keyframes __-in {
+    transition: opacity 1s;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  /* @keyframes __-in {
     from {}
     to {}
-  }
-   */
+  } */
+  
 </style>
